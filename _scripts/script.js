8334a405adf806b4b8bfcb52995dd79f371516f3
Wwinner <fernandowwinner.97@gmail.com>
2021-07-11 03:16:13 +0100

    Ux/Ui Project of settings

---
+++ _scripts/script.js
@@ -0,0 +1,13 @@
+const $ = document.querySelector.bind(this)
+
+//let img =$('avatar')
+let img2 = $('#avatar')
+
+//document.querySelector('#avatar')
+console.log($('#avatar'))
+
+//function apag(){
+   // img.src('jdsjad')
+//}
+
+//window.onload(apag())
